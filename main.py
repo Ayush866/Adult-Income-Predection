7d7dc176a4c63c8762350cb5e26cb8ab7d03cbd3
Ayush866 <ayush786bisht@gmail.com>
2023-05-10 17:10:01 +0530

    added data ingestion

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import pymongo
+from income.utils import get_collection_as_dataframe
+from income.logger import logging
+from income.exception import CustomException
+import sys,os
+from income.entity import config_entity
+from income.components import data_ingestion
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+if __name__=="__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config = training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
 
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          print(e)
